f5b535705ae2b520c613bd98a58a3b78158d1f7b
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2018-03-20 21:34:28 +0100

    Set hero

---
+++ web/themes/custom/dewert_theme/styleguide-src/components/02-molecules/hero/hero.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  title: "Teaser",
+  preview: "@preview",
+  context: {
+    slogan: "Votre spécialiste de la maison en <strong>ossature bois</strong> <strong>écologique</strong> et <strong>basse énergie</strong>.",
+    image: '<img src="https://nosrc.io/1500x670" alt="" />',
+  },
+};
