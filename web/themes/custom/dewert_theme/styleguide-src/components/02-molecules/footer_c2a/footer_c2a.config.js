2c27a21c221cbecce56b4ca81fb5a2baf6ce2e69
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2018-03-21 08:38:23 +0100

    Add modules and footer styles

---
+++ web/themes/custom/dewert_theme/styleguide-src/components/02-molecules/footer_c2a/footer_c2a.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "Teaser",
+  preview: "@preview",
+  context: {
+    title: "Besoin d’un conseil ?<br>Envie de visiter un chantier ?",
+    text: "N’hésitez pas à nous contacter, nous répondons à toutes vos question et nous vous accompagnons volontier pour une visite d’une de nos construction en cours ou achevée.",
+    phone: '<a href="tel:+32494084000">0494/084.000</a>',
+    mail: '<a href="mailto:n.de%77ert@g%6d%61il.com" rel="nofollow">n.dewert@gmail.com</a>',
+    facebook: '<a href="https://fr-fr.facebook.com/public/Nicolas-Dewert">Facebook</a>',
+  },
+};
