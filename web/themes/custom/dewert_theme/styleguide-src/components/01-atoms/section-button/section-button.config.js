8b544b4aa9326d63a613351c0f39a7daa1f619ea
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2018-03-29 09:14:08 +0200

    Update homepage blocks

--- web/themes/custom/dewert_theme/styleguide-src/components/01-atoms/section-button/section-button.config.js
+++ web/themes/custom/dewert_theme/styleguide-src/components/01-atoms/section-button/section-button.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   title: "Section button",
   status: "ready",
+  preview: "@content-section",
   context: {
     text: "Notre expertise",
     url: "#",
--- web/themes/custom/dewert_theme/styleguide-src/components/03-organisms/section-expertise/section-expertise.config.js
+++ web/themes/custom/dewert_theme/styleguide-src/components/03-organisms/section-expertise/section-expertise.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   title: "Section expertise",
+  preview: "@content-section",
   context: {
     title: "Faites construire l’esprit tranquille, nous sommes exigeants avec nous-même",
     image: '<img src="https://nosrc.io/354x677" alt="">',
