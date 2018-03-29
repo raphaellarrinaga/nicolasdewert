724e1bf4201f0beeb23a25baa7421aee37fa3986
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2018-03-29 21:59:52 +0200

    Add expertise block style

--- web/themes/custom/dewert_theme/styleguide-src/components/03-organisms/section-expertise/section-expertise.config.js
+++ web/themes/custom/dewert_theme/styleguide-src/components/03-organisms/section-expertise/section-expertise.config.js
@@ -4,7 +4,6 @@ module.exports = {
   context: {
     title: "Faites construire l’esprit tranquille, nous sommes exigeants avec nous-même",
     image: '<img src="https://nosrc.io/354x677" alt="">',
-    button_text: "Notre expertise",
-    button_url: "#",
+    link: '<a href="#" class="button button--primary"><i class="fa fa-arrow-alt-circle-right"></i> Notre expertise</a>',
   },
 };
