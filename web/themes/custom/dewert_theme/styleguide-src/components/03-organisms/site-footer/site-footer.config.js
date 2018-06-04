e675632436e75cfc04a5f3e5357965627af12da5
Raphael Larrinaga <raphael.larrinaga@gmail.com>
2018-06-04 22:55:31 +0200

    Reformat footer

--- web/themes/custom/dewert_theme/styleguide-src/components/03-organisms/site-footer/site-footer.config.js
+++ web/themes/custom/dewert_theme/styleguide-src/components/03-organisms/site-footer/site-footer.config.js
@@ -3,9 +3,11 @@ module.exports = {
   status: "ready",
   preview: "@preview",
   context: {
-    content : "Footer content",
+    col_1 : "Footer content col1",
+    col_2 : "Footer content col2",
     title : "Besoin d’un conseil ? <br>Envie de visiter un chantier ?",
     text: "N’hésitez pas à nous contacter, nous répondons à toutes vos question et nous vous accompagnons volontier pour une visite d’une de nos construction en cours ou achevée.",
     copyright: "2018 © N.Dewert sprl",
+    image_url: "https://nosrc.io/140x42",
   }
 };
